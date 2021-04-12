af1d8464449b22af402226d380d5f613cc7f1855
Armin <fajkan10@gmail.com>
2021-04-12 09:45:24 +0200

    Added variables in Box and ShippingCostApplication

--- src/main/java/se/lexicon/shipping_cost/entity/Box.java
+++ src/main/java/se/lexicon/shipping_cost/entity/Box.java
@@ -1,4 +1,15 @@
 package se.lexicon.shipping_cost.entity;
 
+import java.time.LocalDateTime;
+
 public class Box {
+    private String id;
+    private String name;
+    private String country;
+    private String type;
+    private double cost;
+    private double weight;
+    private String weightType;
+    private LocalDateTime createDate;
+    private boolean status;
 }
