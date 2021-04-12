88470bf4b11afe1326e8277e4e1feb4bec2fb1e9
Armin <fajkan10@gmail.com>
2021-04-12 10:23:04 +0200

    Added controller mapping and removed fragments

--- src/main/java/se/lexicon/shipping_cost/controller/ShippingController.java
+++ src/main/java/se/lexicon/shipping_cost/controller/ShippingController.java
@@ -1,7 +1,14 @@
 package se.lexicon.shipping_cost.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ShippingController {
+
+    @GetMapping("/")
+    public String indexPage(){
+        return "AddBoxForm";
+    }
+
 }
